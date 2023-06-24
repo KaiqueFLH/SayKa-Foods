11701ef89a6526dea54e6ad68cede9ad9e9a74b8
Kaique Fernandes Lima Homem <kaique_homem@estudante.sesisenai.org.br>
2023-06-24 18:13:36 -0300

    js e formatação do html feitos.

---
+++ script.js
@@ -0,0 +1,19 @@
+function arrastaHome(){
+    window.scrollTo({ left: 0, top: document.body.scrollTop, behavior: "smooth",});
+}
+
+function arrastaProducts(){
+    window.scrollTo({ left: 0, top: 870, behavior: "smooth",});
+}
+
+function arrastaRecipes(){
+    window.scrollTo({ left: 0, top: 1820, behavior: "smooth",});
+}
+
+function arrastaAboutUs(){
+    window.scrollTo({ left: 0, top: 2620, behavior: "smooth",});
+}
+
+function arrastaFooter(){
+    window.scrollTo({ left: 0, top: document.body.scrollHeight, behavior: "smooth",});
+}
